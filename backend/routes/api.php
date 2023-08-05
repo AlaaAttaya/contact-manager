412098b4ba3d11384652228ae825ecab00fda27a
Alaa Attaya <attayaalaa@gmail.com>
2023-08-05 21:51:18 +0300

    Add routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,24 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\AuthController;
 
 Route::group(["middleware" => "auth:api"], function () {
-
     Route::group(["prefix" => "user"], function () {
         Route::get("profile", [AuthController::class, "profile"]);
         Route::post("logout", [AuthController::class, "logout"]);
         Route::post("refresh", [AuthController::class, "refresh"]);
+        Route::post('contacts/store', [AuthController::class, 'store']);
     });
+});
 
+//Unauthenticated APIS
+Route::group(["prefix" => "guest"], function () {
+    //catch api for unauthorized users
+    Route::get("unauthorized", [AuthController::class, "unauthorized"])->name("unauthorized");
+    //login & signup 
+    Route::post("login", [AuthController::class, "login"]);
+    Route::post("register", [AuthController::class, "register"]);
 });
